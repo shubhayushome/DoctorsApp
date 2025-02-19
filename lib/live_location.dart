546a3dcd96e8cb9804f458724ddb4f77ce3c63ba
shubhayushome <123246376+shubhayushome@users.noreply.github.com>
2025-02-19 20:59:49 +0530

    Update live_location.dart

--- lib/live_location.dart
+++ lib/live_location.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+//import 'package:url_launcher/url_launcher.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class LiveLocationListScreen extends StatefulWidget {
   @override
@@ -13,74 +17,91 @@ class _LiveLocationListScreenState extends State<LiveLocationListScreen> {
       'name': 'City Hospital',
       'type': 'Hospital',
       'hours': 'Open 24/7',
-      'latitude': '37.7749',
-      'longitude': '-122.4194',
-      'image': 'https://via.placeholder.com/150',
+      'latitude': '22.653962',
+      'longitude': '88.44513',
+      'image': 'https://i.ytimg.com/vi/ZkeDIPJcV38/maxresdefault.jpg',
     },
     {
       'name': 'Uncle John’s House',
       'type': 'House of Relative',
       'hours': 'Daily 8 AM - 10 PM',
-      'latitude': '34.0522',
-      'longitude': '-118.2437',
-      'image': 'https://via.placeholder.com/150',
+      'latitude': '24.0522',
+      'longitude': '90.2437',
+      'image': 'https://i.ytimg.com/vi/ZkeDIPJcV38/maxresdefault.jpg',
     },
     {
       'name': 'Friend’s Apartment',
       'type': 'House of Friend',
       'hours': 'Mon-Fri 9 AM - 6 PM',
-      'latitude': '40.7128',
-      'longitude': '-74.0060',
-      'image': 'https://via.placeholder.com/150',
+      'latitude': '22.7128',
+      'longitude': '86.0060',
+      'image': 'https://i.ytimg.com/vi/ZkeDIPJcV38/maxresdefault.jpg',
     },
     {
       'name': 'Family Safehouse',
       'type': 'Safe House',
       'hours': 'Daily 6 AM - 8 PM',
-      'latitude': '48.8566',
-      'longitude': '2.3522',
-      'image': 'https://via.placeholder.com/150',
+      'latitude': '21.8566',
+      'longitude': '86.3522',
+      'image': 'https://i.ytimg.com/vi/ZkeDIPJcV38/maxresdefault.jpg',
     },
   ];
 
   // Dummy starting location
-  final String startLatitude = '37.7749';
-  final String startLongitude = '-122.4194';
+  final String startLatitude = '22.586656';
+  final String startLongitude = '88.304115';
 
-  void _undoDelete(Map<String, String> location, int index) {
-    setState(() {
-      safeLocations.insert(index, location);
-    });
-  }
+  void _openDirections(String destinationLatitude, String destinationLongitude,
+      String location_name) async {
+    double? startlat = double.tryParse(startLatitude);
+    double? startlng = double.tryParse(startLongitude);
+    double? destlat = double.tryParse(destinationLatitude);
+    double? destlng = double.tryParse(destinationLongitude);
 
-  void _openDirections(
-      String destinationLatitude, String destinationLongitude) async {
-    final url =
-        'https://www.google.com/maps/dir/?api=1&origin=$startLatitude,$startLongitude&destination=$destinationLatitude,$destinationLongitude&travelmode=driving';
-    if (await canLaunchUrl(Uri.parse(url))) {
-      await launchUrl(Uri.parse(url));
+    if (startlat != null &&
+        startlng != null &&
+        destlat != null &&
+        destlng != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => NavigationMapScreen(
+                startLatitude: startlat,
+                startLongitude: startlng,
+                destinationLatitude: destlat,
+                destinationLongitude: destlng,
+                locationName: location_name)),
+      );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Could not open Google Maps for directions')),
+        const SnackBar(
+          content: Text("Invalid latitude or longitude"),
+        ),
       );
     }
   }
 
   void _openGoogleMaps(BuildContext context) async {
     // User's current location (hardcoded for demo purposes)
-    double currentLat = 37.7749;
-    double currentLng = -122.4194;
+    double? lat = double.tryParse(startLatitude);
+    double? lng = double.tryParse(startLongitude);
 
-    // URL to center the map on the current location and add safe location markers
-    String googleMapsUrl =
-        'https://www.google.com/maps/search/?api=1&query=$currentLat,$currentLng';
-
-    // Launch Google Maps
-    if (await canLaunchUrl(Uri.parse(googleMapsUrl))) {
-      await launchUrl(Uri.parse(googleMapsUrl));
+    if (lat != null && lng != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MapScreen(
+            latitude: lat,
+            longitude: lng,
+            locationName: 'Live Location',
+          ),
+        ),
+      );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Could not open Google Maps.')),
+        const SnackBar(
+          content: Text("Invalid latitude or longitude"),
+        ),
       );
     }
   }
@@ -114,42 +135,10 @@ class _LiveLocationListScreenState extends State<LiveLocationListScreen> {
           itemCount: safeLocations.length,
           itemBuilder: (context, index) {
             final location = safeLocations[index];
-            return Dismissible(
-              key: Key(location['name']!),
-              direction: DismissDirection.endToStart,
-              onDismissed: (direction) {
-                final removedLocation = location;
-                setState(() {
-                  safeLocations.removeAt(index);
-                });
-                ScaffoldMessenger.of(context).clearSnackBars();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('${removedLocation['name']} deleted'),
-                    action: SnackBarAction(
-                      label: 'Undo',
-                      onPressed: () {
-                        _undoDelete(removedLocation, index);
-                      },
-                    ),
-                    duration: Duration(seconds: 4),
-                  ),
-                );
-              },
-              background: Container(
-                color: Colors.red,
-                alignment: Alignment.centerRight,
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Icon(
-                  Icons.delete,
-                  color: Colors.white,
-                ),
-              ),
+            return SizedBox(
               child: GestureDetector(
-                onTap: () => _openDirections(
-                  location['latitude']!,
-                  location['longitude']!,
-                ),
+                onTap: () => _openDirections(location['latitude']!,
+                    location['longitude']!, location['name']!),
                 child: Card(
                   color: Colors.teal[200],
                   margin: EdgeInsets.symmetric(vertical: 8.0),
@@ -212,6 +201,310 @@ class _LiveLocationListScreenState extends State<LiveLocationListScreen> {
               ),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  final double latitude;
+  final double longitude;
+  final String locationName;
+
+  const MapScreen({
+    Key? key,
+    required this.latitude,
+    required this.longitude,
+    required this.locationName,
+  }) : super(key: key);
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  List<Marker> hospitalMarkers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNearbyHospitals();
+  }
+
+  Future<void> fetchNearbyHospitals() async {
+    final query =
+        "[out:json];node[amenity=hospital](around:5000,${widget.latitude},${widget.longitude});out;";
+    final url =
+        Uri.parse('https://overpass-api.de/api/interpreter?data=$query');
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        List<Marker> markers = data['elements'].map<Marker>((element) {
+          return Marker(
+            point: LatLng(element['lat'], element['lon']),
+            width: 40.0,
+            height: 40.0,
+            child: const Icon(
+              Icons.local_hospital,
+              color: Colors.blue,
+              size: 40.0,
+            ),
+          );
+        }).toList();
+
+        setState(() {
+          hospitalMarkers = markers;
+        });
+      }
+    } catch (e) {
+      print("Error fetching hospitals: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Map for ${widget.locationName}'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(widget.latitude, widget.longitude),
+          initialZoom: 12.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+            userAgentPackageName: 'com.example.doctors_app',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(widget.latitude, widget.longitude),
+                width: 40.0,
+                height: 40.0,
+                child: const Icon(
+                  Icons.location_pin,
+                  color: Colors.red,
+                  size: 40.0,
+                ),
+              ),
+              ...hospitalMarkers,
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NavigationMapScreen extends StatefulWidget {
+  final double startLatitude;
+  final double startLongitude;
+  final double destinationLatitude;
+  final double destinationLongitude;
+  final String locationName;
+
+  const NavigationMapScreen({
+    Key? key,
+    required this.startLatitude,
+    required this.startLongitude,
+    required this.destinationLatitude,
+    required this.destinationLongitude,
+    required this.locationName,
+  }) : super(key: key);
+
+  @override
+  _NavigationMapScreenState createState() => _NavigationMapScreenState();
+}
+
+class _NavigationMapScreenState extends State<NavigationMapScreen> {
+  List<List<LatLng>> routes = [];
+  List<double> travelTimes = [];
+  List<LatLng> healthCenters = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchRoutes();
+    _fetchHealthCenters();
+  }
+
+  Future<void> _fetchRoutes() async {
+    final String osrmUrl =
+        'https://router.project-osrm.org/route/v1/driving/${widget.startLongitude},${widget.startLatitude};${widget.destinationLongitude},${widget.destinationLatitude}?alternatives=true&overview=full&geometries=geojson';
+    try {
+      final response = await http.get(Uri.parse(osrmUrl));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          routes = (data['routes'] as List)
+              .map((route) => (route['geometry']['coordinates'] as List)
+                  .map((coord) => LatLng(coord[1], coord[0]))
+                  .toList())
+              .toList();
+          travelTimes = (data['routes'] as List)
+              .map<double>(
+                  (route) => (route['duration'] as num).toDouble() / 60)
+              .toList();
+        });
+      } else {
+        throw Exception("Failed to load routes");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _fetchHealthCenters() async {
+    String overpassQuery =
+        '[out:json];node[amenity=hospital](around:5000,${widget.destinationLatitude},${widget.destinationLongitude});out;';
+    String overpassUrl =
+        'https://overpass-api.de/api/interpreter?data=$overpassQuery';
+
+    try {
+      final response = await http.get(Uri.parse(overpassUrl));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          healthCenters.addAll((data['elements'] as List)
+              .map((node) => LatLng(node['lat'], node['lon'])));
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+    overpassQuery =
+        '[out:json];node[amenity=hospital](around:5000,${widget.startLatitude},${widget.startLongitude});out;';
+    overpassUrl = 'https://overpass-api.de/api/interpreter?data=$overpassQuery';
+
+    try {
+      final response = await http.get(Uri.parse(overpassUrl));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          healthCenters.addAll((data['elements'] as List)
+              .map((node) => LatLng(node['lat'], node['lon'])));
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Color getTrafficColor(double duration) {
+    if (duration > 30) {
+      return Colors.red;
+    } else if (duration > 15) {
+      return Colors.orange;
+    } else {
+      return Colors.blue;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _calculateTextWidth(String text) {
+      TextPainter textPainter = TextPainter(
+        text: TextSpan(
+          text: text,
+          style: TextStyle(fontSize: 12),
+        ),
+        maxLines: 1,
+        textDirection: TextDirection.ltr,
+      )..layout();
+
+      return textPainter.width;
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Route to ${widget.locationName}')),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(widget.startLatitude, widget.startLongitude),
+          initialZoom: 12.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+          ),
+          if (routes.isNotEmpty)
+            PolylineLayer(
+              polylines: List.generate(routes.length, (index) {
+                return Polyline(
+                  points: routes[index],
+                  color: getTrafficColor(travelTimes[index]),
+                  strokeWidth: 5.0,
+                );
+              }),
+            ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(widget.startLatitude, widget.startLongitude),
+                width: 40.0,
+                height: 40.0,
+                child: const Icon(Icons.location_pin,
+                    color: Colors.green, size: 40.0),
+              ),
+              Marker(
+                point: LatLng(
+                    widget.destinationLatitude, widget.destinationLongitude),
+                width: 40.0,
+                height: 40.0,
+                child: const Icon(Icons.location_pin,
+                    color: Colors.red, size: 40.0),
+              ),
+              for (int i = 0; i < routes.length; i++)
+                Marker(
+                  point: routes[i][routes[i].length ~/ 2],
+                  width: _calculateTextWidth(
+                          "${travelTimes[i].toStringAsFixed(1)} min") +
+                      16,
+                  height: 30,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text(
+                      "${travelTimes[i].toStringAsFixed(1)} min",
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  ),
+                ),
+              for (LatLng center in healthCenters)
+                Marker(
+                  point: center,
+                  width: 30.0,
+                  height: 30.0,
+                  child: const Icon(Icons.local_hospital,
+                      color: Colors.blue, size: 30.0),
+                ),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(16),
+        color: Colors.teal,
+        child: Text(
+          "Multiple Routes Available - Estimated Travel Time Varies",
+          style: const TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+          textAlign: TextAlign.center,
         ),
       ),
     );
